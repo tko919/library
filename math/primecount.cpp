1eb9eb409bf141d9897fa08529848f7628cba1b5
tko919 <61387362+tko919@users.noreply.github.com>
2020-04-01 17:46:31 +0900

    Update primecount.cpp

--- math/primecount.cpp
+++ math/primecount.cpp
@@ -1,3 +1,18 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+//template
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define rrep(i,a,b) for(int i=(a);i>(b);i--)
+#define ALL(v) (v).begin(),(v).end()
+typedef long long int ll;
+const int inf = 0x3fffffff; const ll INF = 0x1fffffffffffffff; const double eps=1e-12;
+void tostr(ll x,string& res){while(x)res+=('0'+(x%10)),x/=10; reverse(ALL(res)); return;}
+template<class T> inline bool chmax(T& a,T b){ if(a<b){a=b;return 1;}return 0; }
+template<class T> inline bool chmin(T& a,T b){ if(a>b){a=b;return 1;}return 0; }
+//end
+
 constexpr int lim=1341398;
 constexpr int mul[]={2,6,30,210,2310,30030,510510};
 constexpr short sz=256;
@@ -47,3 +62,10 @@ ll pi(ll x){
       }
    } return pi_memo[x]=res;
 } //need to initialize
+
+int main(){
+   init();
+   ll n; cin>>n;
+   cout<<pi(n)<<endl;
+   return 0;
+}
