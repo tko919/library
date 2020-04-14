77dbaec8e496413bddf1adf71482660b5537b9ea
tko919 <61387362+tko919@users.noreply.github.com>
2020-04-15 00:13:12 +0900

    Update primecount.cpp

--- math/primecount.cpp
+++ math/primecount.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 //template
 #define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define rrep(i,a,b) for(int i=(a);i>(b);i--)
 #define ALL(v) (v).begin(),(v).end()
 typedef long long int ll;
 const int inf = 0x3fffffff; const ll INF = 0x1fffffffffffffff; const double eps=1e-12;
@@ -12,40 +13,72 @@ template<class T> inline bool chmax(T& a,T b){ if(a<b){a=b;return 1;}return 0; }
 template<class T> inline bool chmin(T& a,T b){ if(a>b){a=b;return 1;}return 0; }
 //end
 
-ll pi(ll n){
-   if(n<=1)return 0;
-   const int v=sqrtl(n); int s=(v+1)/2;
-   vector<int> ss(v+1),rs(s); vector<ll> ls(s);
-   rep(i,2,v+1)ss[i]=(i+1)/2;
-   rep(i,0,s)rs[i]=i*2+1,ls[i]=(n/rs[i]+1)/2;
-   vector<bool> skip(v+1); int cnt=0;
-   for(int p=3;p<=v;p++)if(ss[p]>ss[p-1]){
-      int q=p*p; cnt++;
-      if(1LL*q*q>n)break; skip[p]=1;
-      for(int i=q;i<=v;i+=p*2)skip[i]=1;
-      int nxt=0;
-      for(int k=0;k<s;k++){
-         int i=rs[k]; if(skip[i])continue;
-         ll d=1LL*p*i;
-         ls[nxt]=ls[k]-(d<=v?ls[ss[d]-cnt]:ss[n/d])+cnt;
-         rs[nxt++]=i;
-      } s=nxt;
-      for(int j=v/p;j>=p;j--){
-         int c=ss[j]-cnt;
-         for(int i=p*j,lim=min(p+i,v+1);i<lim;i++)ss[i]-=c;
-      }
+struct BIT{
+   int n; vector<int> val;
+   BIT(int _n):n(_n),val(_n+1,0){}
+   void add(int i,int x){for(i++;i<=n;i+=i&-i)val[i]+=x;}
+   int sum(int i){int res=0; for(i++;i;i-=i&-i)res+=val[i]; return res;}
+};
+
+constexpr int R=1010101;
+constexpr int C=10101;
+bitset<R> isp; vector<int> ps,cs;
+
+void init(int c){
+   ps.clear(); cs.clear();
+   rep(i,2,R+1)isp[i]=1;
+   for(int p=2;p*p<=R;p++)if(isp[p]){
+      for(int q=p*p;q<=R;q+=p)isp[q]=0;
    }
-   for(int k=1;k<s;k++){
-      const ll m=n/rs[k]; ll t=ls[k]-(cnt+k-1);
-      for(int l=1;l<k;l++){
-         int p=rs[l]; if(1LL*p*p>m)break;
-         t-=ss[m/p]-(cnt+l-1);
-      } ls[0]-=t;
-   } return ls[0];
+   rep(i,2,R+1)if(isp[i]){
+      ps.push_back(i);
+      if(i<=c)cs.push_back(i);
+   }
+}
+
+ll phi(ll x,ll a){
+   ll res=0;
+   vector<int> mu(a+1,1),minp(a+1,a); int cnt=0;
+   rep(i,1,a+1){
+      if(isp[i]){
+         for(ll j=i;j<=a;j+=i){mu[j]*=-1; chmin(minp[j],cnt);}
+         for(ll j=i*i,k=j;k<=a;k+=j)mu[k]=0; cnt++;
+      } res+=mu[i]*(x/i);
+   }
+   vector<ll> sum(cnt,0);
+   for(ll lo=1;lo<x/a;lo+=a){
+      ll hi=min(lo+a,x/a);
+      BIT bit(a); bitset<C> is_one;
+      rep(i,0,a)bit.add(i,1),is_one[i]=1;
+      rep(b,0,cnt){
+         int p=cs[b],mi=max(x/p/hi,a/p),ma=min(x/p/lo,a);
+         if(p<ma){
+            rrep(m,ma,mi)if(mu[m]!=0 and minp[m]>b){
+               res-=mu[m]*(sum[b]+bit.sum(x/p/m-lo));
+            }
+         } sum[b]+=bit.sum(a-1);
+         for(int q=(p-lo%p)%p;q<a;q+=p)if(is_one[q]){
+            bit.add(q,-1); is_one[q]=0;
+         }
+      }
+   } return res;
+}
+
+ll pi(ll x){
+   int r=sqrt(x),c=cbrt(x); init(c);
+   if(x<=R)return upper_bound(ALL(ps),x)-ps.begin();
+   ll a=upper_bound(ALL(ps),c)-ps.begin(),b=upper_bound(ALL(ps),r)-ps.begin();
+   ll res=phi(x,c)+(b+a-2)*(b-a+1)/2; int idx=b-1;
+   for(int s=r;s<=x and idx>=a;s+=c){
+      vector<ll> cur(c+1,0); bitset<C> val; cur[0]=b; rep(i,0,C)val[i]=1;
+      for(int p:cs)for(int q=p*(s/p);q<=s+c;q+=p)if(q>=s)val[q-s]=0;
+      rep(i,1,c+1)cur[i]=cur[i-1]+val[i]; b=cur[c];
+      while(s<=x/ps[idx] and x/ps[idx]<s+c and idx>=a){res-=cur[x/ps[idx]-s]; idx--;}
+   } return res;
 }
 
 int main(){
    ll n; cin>>n;
-   printf("%lld\n",pi(n));
+   cout<<pi(n)<<endl;
    return 0;
 }
