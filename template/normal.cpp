6387caf087b866cb5179670b96c44173e52d9287
tko919 <61387362+tko919@users.noreply.github.com>
2021-01-31 00:03:56 +0900

    Create normal.cpp

---
+++ template/normal.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+//template
+#define rep(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define ALL(v) (v).begin(),(v).end()
+using ll=long long int;
+const int inf = 0x3fffffff; const ll INF = 0x1fffffffffffffff; const double eps=1e-12;
+template<typename T>inline bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+template<typename T>inline bool chmin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
+//end
+
+
+
+int main(){
+   
+   return 0;
+}
