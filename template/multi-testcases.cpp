af1685e638bf35906cf0c88f20d3317658b01ea4
tko919 <61387362+tko919@users.noreply.github.com>
2021-01-31 00:04:33 +0900

    Create multi-testcases.cpp

---
+++ template/multi-testcases.cpp
@@ -0,0 +1,23 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+//template
+#define rep(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define ALL(v) (v).begin(),(v).end()
+using ll=long long int;
+const int inf = 0x3fffffff; const ll INF = 0x1fffffffffffffff; const double eps=1e-12;
+template<typename T>inline bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+template<typename T>inline bool chmin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
+//end
+
+void solve(int _rot){
+   printf("Case #%d: ",_rot);
+   
+}
+
+int main(){
+   int t; scanf("%d",&t);
+   rep(rot,0,t)solve(rot+1);
+   return 0;
+}
