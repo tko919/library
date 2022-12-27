24870191901bcc1bd75e3298229be65cd4d99f60
tko919 <laplace2758@gmail.com>
2022-12-28 03:41:15 +0900

    Fix

--- Math/floorsum.hpp
+++ Math/floorsum.hpp
@@ -1,24 +1,22 @@
 #pragma once
-#include "Math/fastdiv.hpp"
 
 ll FloorSum(ll n,ll a,ll b,ll m){
    //sum_{k=0}^{n-1} [(a*k+b)/m]
-   FastDiv im(m);
    ll res=0;
-   if(a>=m)res-=(a/im)*n*(n-1)/2,a-=a/im*m;
-   else if(a<0)res+=((-a+m-1)/im)*n*(n-1)/2,a+=((-a+m-1)/im)*m;
-   if(b>=m)res-=(b/im)*n,b-=b/im*m;
-   else if(b<0)res+=((-b+m-1)/im)*n,b+=((-b+m-1)/im)*m;
+   if(a>=m)res-=(a/m)*n*(n-1)/2,a-=a/m*m;
+   else if(a<0)res+=((-a+m-1)/m)*n*(n-1)/2,a+=((-a+m-1)/m)*m;
+   if(b>=m)res-=(b/m)*n,b-=b/m*m;
+   else if(b<0)res+=((-b+m-1)/m)*n,b+=((-b+m-1)/m)*m;
    
    while(1){
       ll y_max=a*n+b;
       if(y_max<m)break;
-      n=y_max/im;
-      b=y_max%im;
+      n=y_max/m;
+      b=y_max%m;
       res+=(n*(n-1)/2)*(m/a)+n*(b/a);
       swap(m,a);
-      a=a%im;
-      b=b%im;
+      a=a%m;
+      b=b%m;
    }
    return res;
 }
