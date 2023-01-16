8c9daf86686efa6714b4548cf6af32ceeea05de2
tko919 <laplace2758@gmail.com>
2023-01-17 02:37:48 +0900

    Fix

--- Template/template.hpp
+++ Template/template.hpp
@@ -19,4 +19,4 @@ template<typename T,typename U>T ceil(T x,U y){assert(y!=0); if(y<0)x=-x,y=-y; r
 template<typename T,typename U>T floor(T x,U y){assert(y!=0); if(y<0)x=-x,y=-y; return (x>0?x/y:(x-y+1)/y);}
 template<typename T>int popcnt(T x){return __builtin_popcountll(x);}
 template<typename T>int topbit(T x){return (x==0?-1:63-__builtin_clzll(x));}
-template<typename T>int lowbit(T x){return (x==0?-1:63-__builtin_clzll(x));}
+template<typename T>int lowbit(T x){return (x==0?-1:__builtin_ctzll(x));}
