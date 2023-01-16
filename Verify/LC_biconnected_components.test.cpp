2b38acc22413ca1f4c18fd5c3df54fb9204ecf75
tko919 <laplace2758@gmail.com>
2023-01-16 21:15:41 +0900

    Fix

--- Verify/LC_biconnected_components.test.cpp
+++ Verify/LC_biconnected_components.test.cpp
@@ -9,17 +9,33 @@ int main(){
     int n,m;
     io.read(n,m);
     BiconnectedComponents bc(n);
+    using P=pair<int,int>;
+    vector<P> es(m);
     rep(_,0,m){
         int x,y;
         io.read(x,y);
         bc.add_edge(x,y);
+        es[_]={x,y};
     }
     bc.run();
     auto ret=bc.bc;
-    io.write(int(ret.size()));
+    vector<int> used(n,0);
+    vector<vector<int>> res;
     for(auto& v:ret){
-        io.write(int(v.size()));
-        io.write(v);
+        vector<int> w;
+        for(auto& id:v){
+            w.push_back(es[id].first);
+            w.push_back(es[id].second);
+        }
+        sort(ALL(w));
+        w.erase(unique(ALL(w)),w.end());
+        for(auto& x:w)used[x]=1;
+        res.push_back(w);
+    }
+    rep(i,0,n)if(!used[i])res.push_back({i});
+    io.write((int)res.size());
+    for(auto& v:res){
+        io.write((int)v.size(),v);
     }
     return 0;
 }
