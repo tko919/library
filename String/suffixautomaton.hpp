605d9483df08163616390fb6e64872e35c23d96c
tko919 <laplace2758@gmail.com>
2024-06-28 21:40:24 +0900

    Add

--- String/suffixautomaton.hpp
+++ String/suffixautomaton.hpp
@@ -7,15 +7,16 @@ struct SuffixAutomaton {
         Node(int L = 0) : link(-1), len(L) {}
     };
     vector<Node> Nodes;
-    int last;
+    int last, count;
     SuffixAutomaton() {
         Nodes.push_back(Node());
         Nodes.back().origin = 0;
-        last = 0;
+        last = count = 0;
     }
-    void push(char c) {
+    void push(int c) {
         int nlast = Nodes.size();
         Nodes.push_back(Node(Nodes[last].len + 1));
+        count += Nodes[last].len + 1;
         Nodes.back().origin = nlast;
         int cur = last;
         while (cur != -1 and !Nodes[cur].nxt.count(c)) {
@@ -26,14 +27,25 @@ struct SuffixAutomaton {
             Nodes[nlast].link = 0;
         else {
             int to = Nodes[cur].nxt[c];
-            if (Nodes[to].len == Nodes[cur].len + 1)
+            if (Nodes[to].len == Nodes[cur].len + 1) {
                 Nodes[nlast].link = to;
-            else {
+                count -= Nodes[to].len;
+            } else {
                 int extra = Nodes.size();
                 Nodes.push_back(Nodes[to]);
                 Nodes.back().len = Nodes[cur].len + 1;
                 Nodes.back().origin = to;
+
+                count += Nodes[Nodes[to].link].len;
+                if (Nodes[nlast].link != -1)
+                    count += Nodes[Nodes[nlast].link].len;
+
                 Nodes[to].link = Nodes[nlast].link = extra;
+
+                count -= Nodes[Nodes[to].link].len;
+                count -= Nodes[Nodes[nlast].link].len;
+                count += Nodes.back().len - Nodes[Nodes.back().link].len;
+
                 while (cur != -1 and Nodes[cur].nxt[c] == to) {
                     Nodes[cur].nxt[c] = extra;
                     cur = Nodes[cur].link;
