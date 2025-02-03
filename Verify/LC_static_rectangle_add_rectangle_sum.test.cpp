711fdac4ba983a32bbf7b1b9e874456722b02362
tko919 <laplace2758@gmail.com>
2025-02-04 02:14:29 +0900

    Fix

--- Verify/LC_static_rectangle_add_rectangle_sum.test.cpp
+++ Verify/LC_static_rectangle_add_rectangle_sum.test.cpp
@@ -1,29 +1,30 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/static_rectangle_add_rectangle_sum"
+#define PROBLEM                                                                \
+    "https://judge.yosupo.jp/problem/static_rectangle_add_rectangle_sum"
 
 #include "Template/template.hpp"
 #include "Utility/fastio.hpp"
 
 #include "Math/modint.hpp"
-using Fp=fp<998244353>;
+using Fp = fp<998244353>;
 
 #include "DataStructure/staticrectaddrectsum.hpp"
 
-int main(){
-    int n,q;
-    read(n,q);
+int main() {
+    int n, q;
+    read(n, q);
 
-    StaticRectangleAddRectangleSum<Fp> buf;
-    rep(i,0,n){
-        int L,D,R,U,w;
-        read(L,D,R,U,w);
-        buf.add(L,D,R,U,w);
+    StaticRectangleAddRectangleSum<int, Fp> buf;
+    rep(i, 0, n) {
+        int L, D, R, U, w;
+        read(L, D, R, U, w);
+        buf.add(L, D, R, U, w);
     }
-    rep(i,0,q){
-        int L,D,R,U;
-        read(L,D,R,U);
-        buf.query(L,D,R,U);
+    rep(i, 0, q) {
+        int L, D, R, U;
+        read(L, D, R, U);
+        buf.query(L, D, R, U);
     }
-    auto ret=buf.run();
-    rep(i,0,q)print(ret[i].v);
+    auto ret = buf.run();
+    rep(i, 0, q) print(ret[i].v);
     return 0;
 }
